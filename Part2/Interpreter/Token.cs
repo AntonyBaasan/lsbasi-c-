c134ec706ce2a1d52345ca8a623e8cbcdd876c6e
Antony Baasandorj <antony.baasan@gmail.com>
2018-10-09 12:25:29 -0400

    add part2 template

---
+++ Part2/Interpreter/Token.cs
@@ -0,0 +1,20 @@
+﻿namespace Interpreter
+{
+    class Token
+    {
+        public Token(TokenType type, string value)
+        {
+            TokenType = type;
+            Value = value;
+        }
+
+        public TokenType TokenType { get; }
+
+        public string Value { get; }
+
+        public override string ToString()
+        {
+            return $"Token({TokenType.ToString()}, {Value}";
+        }
+    }
+}
