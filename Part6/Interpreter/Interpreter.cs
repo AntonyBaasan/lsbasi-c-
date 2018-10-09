f720188520d74e19efc35648fbbb53797ef0fabc
Antony Baasandorj <antony.baasan@gmail.com>
2018-10-09 14:59:35 -0400

    add part 6. Parentheses

---
+++ Part6/Interpreter/Interpreter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Interpreter
+{
+    public class Interpreter
+    {
+        private Token currentToken;
+        private readonly Lexer lexer;
+
+        public Interpreter(Lexer lexer)
+        {
+            this.lexer = lexer;
+            currentToken = lexer.GetNextToken();
+        }
+
+        private void Error()
+        {
+            throw new Exception("Error input string");
+        }
+
+        private int Factor()
+        {
+            var token = currentToken;
+            if (token.TokenType == TokenType.INTEGER)
+            {
+                Eat(TokenType.INTEGER);
+                return int.Parse(token.Value);
+            }
+            else if (token.TokenType == TokenType.LPAREN)
+            {
+                Eat(TokenType.LPAREN);
+                var result = Expr();
+                Eat(TokenType.RPAREN);
+                return result;
+            }
+            Error();
+
+            return -1;
+        }
+
+        private int Term()
+        {
+            var result = Factor();
+
+            while (currentToken.TokenType == TokenType.MUL || currentToken.TokenType == TokenType.DIV)
+            {
+                var token = currentToken;
+                if (token.TokenType == TokenType.MUL)
+                {
+                    Eat(TokenType.MUL);
+                    result *= Factor();
+                }
+                if (token.TokenType == TokenType.DIV)
+                {
+                    Eat(TokenType.DIV);
+                    result /= Factor();
+                }
+            }
+
+            return result;
+        }
+
+        private void Eat(TokenType tokenType)
+        {
+            if (currentToken.TokenType == tokenType)
+            {
+                currentToken = this.lexer.GetNextToken();
+            }
+            else
+            {
+                Error();
+            }
+        }
+
+        public int Expr()
+        {
+            int result = Term();
+
+            while (currentToken.TokenType == TokenType.PLUS || currentToken.TokenType == TokenType.MINUS)
+            {
+                var token = currentToken;
+                if (token.TokenType == TokenType.PLUS)
+                {
+                    Eat(TokenType.PLUS);
+                    result += Term();
+                }
+                if (token.TokenType == TokenType.MINUS)
+                {
+                    Eat(TokenType.MINUS);
+                    result -= Term();
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
